9444291337bf63dd36c57f83e39c4407a7b50434
Cristian_Gomez26 <porte20k7@gmail.com>
2022-07-10 17:21:44 -0300

    tercer repo
    mi tercer repositorio de JS

---
+++ variables.js
@@ -0,0 +1,74 @@
+'use strict' // Modo estricto de JavaScript
+
+//Declaramos Nombre Y Apellido
+var nombre = "Cristian Carlos Nicolas"
+var apellido = "Gomez"
+
+//Alerta Del Nombre Y Apellido
+alert(nombre+" "+apellido)
+
+//Declaramos la Edad y lo imprimimos en el documento
+var edad = 25
+document.write("EDAD: "+edad+" "+"AÑOS")
+
+//Sumamos la edad con otro número
+var numero
+var edad 
+numero = 5
+edad = 25
+
+var resultado = numero + edad
+
+console.log(resultado)
+
+
+var a, b, c, d, eTexto;
+a = 3;
+b = 5;
+c = true;
+d = false;
+eTexto = '1'
+
+var resultado = (a + b == 8 && a - b == 1);
+alert("a + b == 8 && a - b == 1 \n "+resultado);
+
+var resultado2 = (a + b == 8 && a - b == -2);
+alert("a + b == 8 && a - b == -2 \n"+resultado2)
+
+var resultado3 = c == d
+alert(resultado3)
+
+var resultado4 = (c && d)
+alert(resultado4)
+ 
+var resultado5 = c || d
+alert(resultado5)
+
+var resultado6 = !a
+alert(resultado6)
+
+var resultado7 = eTexto == 1
+alert(resultado7)
+
+
+var e = "Zapato"
+var f = "avellano"
+
+var resultado8 = e < f
+alert(resultado8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
